92143c487d6eb8c9f7b865d4fa828dde33452f11
Christian Aldridge <Studiozut@gmail.com>
2019-01-09 14:13:56 -0500

    Text change for notice

--- gdpr_notice/cookie.notice.js
+++ gdpr_notice/cookie.notice.js
@@ -18,10 +18,8 @@
      */
     var defaults = {
         messageLocales: {
-            it: 'Utilizziamo i cookie per essere sicuri che tu possa avere la migliore esperienza sul nostro sito. Se continui ad utilizzare questo sito assumiamo che tu ne sia felice.',
             en: 'This website uses cookies to provide you with an improved user experience. By continuing to browse this site, you consent to the use of cookies and similar technologies. Please visit our <a href="/about/privacy-policy">privacy policy</a> for further details.',
-            de: 'Wir verwenden Cookies um sicherzustellen, dass Sie das beste Erlebnis auf unserer Website haben.',
-            fr: 'Nous utilisons des cookies afin d\'être sûr que vous pouvez avoir la meilleure expérience sur notre site. Si vous continuez à utiliser ce site, nous supposons que vous acceptez.'
+            fr: 'Nous utilisons des cookies afin d\'être sûr que vous pouvez avoir la meilleure expérience sur notre site. Si vous continuez à utiliser ce site, nous supposons que vous acceptez l\'utilisation de cookies et technologies similaires. Visitez notre <a href="/about/privacy-policy">politique de confidentialité</a> pour des informations additionelles.'
         },
 
         cookieNoticePosition: 'bottom',
